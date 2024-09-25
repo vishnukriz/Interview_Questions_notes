d61bcea24a64570a846e3136b9bec8cd95b64cea
Vishnuvardhini <101253723+vishnukriz@users.noreply.github.com>
2024-09-25 18:09:01 +0530

    Update 9.printing-Prime.js

--- code/9.printing-Prime.js
+++ code/9.printing-Prime.js
@@ -20,3 +20,20 @@ prime();
 5
 7
 **/
+
+// performance time will be minimum
+function isprime(n){ 
+    for( let i=2;i<=Math.floor(Math.sqrt(n));i++){
+       
+        if(n%i==0)return false}
+    return true
+}
+function printPrimt(n){
+    for (let i=2;i<=n;i++)
+    if(isprime(i))console.log(i)
+}
+var startTime = performance.now()
+printPrimt(10000000)
+var endTime = performance.now()
+
+console.log(`Call to doSomething took ${endTime - startTime} milliseconds`)
